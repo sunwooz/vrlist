8561a7963902e5a3a5d0dcfc011ae2c20d588275
sunwooz <yangsunwoo@gmail.com>
2016-05-21 20:13:10 -0400

    validate uniqueness of game name

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,5 @@ class Game < ActiveRecord::Base
 
   has_attached_file :main_image, styles: { large: "1555×1037", medium: "394x262#" }
   validates_attachment_content_type :main_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_uniqueness_of :name
 end
