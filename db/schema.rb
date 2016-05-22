af4e69eafa858b63df326800169ec600372acdc4
sunwooz <yangsunwoo@gmail.com>
2016-05-22 18:28:27 -0400

    add website

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522191633) do
+ActiveRecord::Schema.define(version: 20160522221310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160522191633) do
     t.datetime "main_image_updated_at"
     t.text     "detail"
     t.string   "category"
+    t.string   "website"
   end
 
   create_table "taggings", force: :cascade do |t|
