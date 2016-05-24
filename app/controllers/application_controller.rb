4694fb31ac18367fa998db1bd11e95b3052c6796
sunwooz <yangsunwoo@gmail.com>
2016-05-23 22:37:06 -0400

    block ip address comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #block ip address methods
+  # before_filter :block_ip_addresses
+
+  # protected
+
+  # def block_ip_addresses
+  #   head :unauthorized if current_ip_address == "XX.XX.XX.XX"
+  # end
+
+  # def current_ip_address
+  #   request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
+  # end
 end
