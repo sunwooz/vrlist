94042f038533f57fc2de3b1547f1d17c4e3c7373
sunwooz <yangsunwoo@gmail.com>
2016-05-31 15:30:10 -0400

    use friendly_id in controllers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -50,14 +50,14 @@ class GamesController < ApplicationController
   end
 
   def update
-    @game = Game.find(params[:id])
+    @game = Game.friendly.find(params[:id])
     if @game.update(game_params)
       redirect_to @game
     end
   end
 
   def destroy
-    @game = Game.find(params[:id])
+    @game = Game.friendly.find(params[:id])
     if @game.destroy
       redirect_to games_path
     end
